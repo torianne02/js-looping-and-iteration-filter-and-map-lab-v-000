27712c18abcfef13806f7a73f9e25130bca30f16
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-19 00:13:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function driversWithRevenueOver(driver, revenue) {
-  return driver.filter(function (driver) {
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (driver) {
     return driver.revenue > revenue;
   });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, )
 }
